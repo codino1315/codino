a13ab2680d8942a9b69dddff6847e50ab93351e4
Prahlad Nayak <pnayak1501@gmail.com>
2021-10-05 08:51:56 +0530

    Update script.js

--- codino/js/script.js
+++ codino/js/script.js
@@ -1,4 +1,4 @@
-
+// What is classic editor? Image result for classic editor. Classic Editor is an official plugin maintained by the WordPress team that restores the previous (“classic”) WordPress editor and the “Edit Post” screen.
     ClassicEditor
         .create( document.querySelector( '#body' ) )
         .catch( error => {
